d94dcb445d40a8910560b2109754d020eec73a6d
Ubuntu User <ubuntu@tk2-238-28667.vs.sakura.ne.jp>
2017-05-30 00:32:03 +0800

    daily commit

---
+++ src/main/java/com/ucap/duanwu/App.java
@@ -0,0 +1,13 @@
+package com.ucap.duanwu.App;
+
+import com.ucap.duanwu.htmlpage.HtmlPage;
+import com.ucap.duanwu.htmlpage.PageParser;
+import prsn.sayid.duanwu.htmlpage.PageParserSayidImp;
+
+public class App
+{
+    public static void main(String args[]) throws Exception
+    {
+        PageParser pageParser = HtmlPage.makePageParser();
+    }
+}
